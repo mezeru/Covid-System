0480ac020e56a3c10f29b993d28b0839dd349fe3
Yash <jermpro0@gmail.com>
2022-01-14 18:33:34 +0530

    FHIR Resource encounter class updated

--- src/components/resouces/fhirEncounter.ts
+++ src/components/resouces/fhirEncounter.ts
@@ -1,14 +1,26 @@
-export const encounterResource = (AdhaarNo,fhirPatientId) => {
+
+
+
+export const encounterResource = (AdhaarNo, fhirPatientId, classNo) => {
+
+    const classArr = [
+        {
+            system: "http://terminology.hl7.org/CodeSystem/v3-ActCode", // OPD
+            code: "AMB",
+            display: "Ambulatory"
+        },
+        {
+            system: "http://terminology.hl7.org/CodeSystem/v3-ActCode", // Assessment
+            code: "CDSREV",
+            display: "Assessment "
+        }
+    ]
 
     const resource = {
         resourceType: "Encounter",
         identifier : [{value: AdhaarNo} ],
         status: "finished",
-        class: {
-            system: "http://terminology.hl7.org/CodeSystem/v3-ActCode",
-            code: "AMB",
-            display: "ambulatory"
-        },
+        class: classArr[classNo],
         subject: {
             reference: `Patient/${fhirPatientId}`
         },
