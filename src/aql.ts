d27a4e591b2f506264437c40d016ecf5e261f9c2
Yash <jermpro0@gmail.com>
2022-02-15 14:19:10 +0530

    Vitals made universal

--- src/aql.ts
+++ src/aql.ts
@@ -39,12 +39,16 @@ export const compositionsList = async (ehrId :string) =>{
   export const Vitals = async (ehrId :string) => {
     const query = `SELECT
     c/context/start_time as Time,
-    c/content[openEHR-EHR-SECTION.adhoc.v1,'Clinical Background']/items[openEHR-EHR-ADMIN_ENTRY.covid_19_admission.v0]/data[at0001]/items[at0002]/value as Admit,
-    c/content[openEHR-EHR-SECTION.adhoc.v1,'Clinical Background']/items[openEHR-EHR-SECTION.adhoc.v1,'Vital Signs']/items[openEHR-EHR-OBSERVATION.body_temperature.v2]/data[at0002]/events[at0003]/data[at0001]/items[at0004]/value as Temperature,
-    c/content[openEHR-EHR-SECTION.adhoc.v1,'Clinical Background']/items[openEHR-EHR-SECTION.adhoc.v1,'Vital Signs']/items[openEHR-EHR-OBSERVATION.respiration.v2]/data[at0001]/events[at0002]/data[at0003]/items[at0062]/value as Pulse_Presence,
-    c/content[openEHR-EHR-SECTION.adhoc.v1,'Clinical Background']/items[openEHR-EHR-SECTION.adhoc.v1,'Vital Signs']/items[openEHR-EHR-OBSERVATION.respiration.v2]/data[at0001]/events[at0002]/data[at0003]/items[at0004]/value as Pulse_Rate,
-    c/content[openEHR-EHR-SECTION.adhoc.v1,'Clinical Background']/items[openEHR-EHR-SECTION.adhoc.v1,'Vital Signs']/items[openEHR-EHR-OBSERVATION.pulse_oximetry.v1]/data[at0001]/events[at0002]/data[at0003]/items[at0006]/value as SpO2
-    from EHR e CONTAINS COMPOSITION c CONTAINS OBSERVATION o
+    a/data[at0001]/items[at0002]/value as Admit,
+    o1/data[at0002]/events[at0003]/data[at0001]/items[at0004]/value as Temperature,
+    o2/data[at0001]/events[at0002]/data[at0003]/items[at0062]/value as Pulse_Presence,
+    o2/data[at0001]/events[at0002]/data[at0003]/items[at0004]/value as Pulse_Rate,
+    o3/data[at0001]/events[at0002]/data[at0003]/items[at0006]/value as SpO2
+    from EHR e CONTAINS COMPOSITION c CONTAINS 
+    OBSERVATION o1 [openEHR-EHR-OBSERVATION.body_temperature.v2] OR 
+    OBSERVATION o2 [openEHR-EHR-OBSERVATION.respiration.v2] OR 
+    OBSERVATION o3 [openEHR-EHR-OBSERVATION.pulse_oximetry.v1] OR
+    ADMIN_ENTRY a [openEHR-EHR-ADMIN_ENTRY.covid_19_admission.v0]
     WHERE e/ehr_id/value='${ehrId}'
     LIMIT 10
     ORDER by Time DESC
